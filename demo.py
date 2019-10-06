ad490241c628e50c527c02d7391951f1b83ba4df
Ubuntu <ubuntu@ip-172-31-45-175.us-east-2.compute.internal>
2019-10-06 08:00:16 +0000

    Update: Rotation added

--- demo.py
+++ demo.py
@@ -3,8 +3,6 @@ deal with multiple people
 images with openpose output
 
 """
-
-
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
@@ -12,6 +10,7 @@ from __future__ import print_function
 import sys
 from absl import flags
 import numpy as np
+import math
 
 import skimage.io as io
 import tensorflow as tf
@@ -35,29 +34,47 @@ flags.DEFINE_string(
 flags.DEFINE_integer('num_people', None, 
     'Number of people in the image.')
 
-
-def calibration(proc_params):
+def calibration(proc_params, frameno, i, n):
+    import matplotlib.pyplot as plt
+    
     img_size = proc_params['img_size']
     undo_scale = 1. / np.array(proc_params['scale'])
     principal_pt = np.array([img_size, img_size]) / 2.
     start_pt = proc_params['start_pt'] - 0.5 * img_size
     final_principal_pt = (principal_pt + start_pt) * undo_scale
     pp = final_principal_pt
+    
+    p = -1
+    if frameno == 1:
+        p = i
+    else:
+        # iterate through the csv files of the previous frame, looking for closest person in that frame.
+        dist = float('inf')
+        for j in range(n):
+            prev_center = np.loadtxt('cali/center/prev_center_%03d_%02d.csv'%(frameno-1, j))
+                        
+            tmp_dist = distance(prev_center, pp)
+            
+            if dist > tmp_dist:
+                dist = tmp_dist
+                p = j
+    
+    np.savetxt('cali/center/prev_center_%03d_%02d.csv' % (frameno, p), pp, delimiter=',')
 
-    H = np.loadtxt("homography.csv",delimiter=",")
+    H = np.loadtxt("cali/homography.csv",delimiter=",")
     try : 
-        diff = np.loadtxt("diff.csv",delimiter=",")
+        diff = np.loadtxt("cali/diff.csv",delimiter=",")
     except :
-        cross_point = np.loadtxt("crosspoint.csv",delimiter=",")
+        cross_point = np.loadtxt("cali/crosspoint.csv",delimiter=",")
         diff = np.array([pp[0] - cross_point[0],pp[1] - cross_point[1]])
-        np.savetxt("diff.csv",diff,delimiter=",")
+        np.savetxt("cali/diff.csv",diff,delimiter=",")
+    
     src = np.array([[pp[0]-diff[0]],[pp[1]-diff[1]],[1]])
     tar = np.matmul(H,src)[:-1].T
-    return (tar[0][0],tar[0][1])
+        
+    return -tar[0][0],-tar[0][1], p
 
 
-
-# TODO get multiple scale and center and get preprocessed
 def preprocess_image(img_path, json_path, n):
     img = io.imread(img_path)
     if img.shape[2] == 4:
@@ -82,21 +99,29 @@ def preprocess_image(img_path, json_path, n):
 def main(img_path, json_path, n):
     sess = tf.Session()
     model = RunModel(config, sess=sess)
-
-    # TODO get multiple cropped images and get estimation for each of them
+    
     input_imgs, proc_params, img = preprocess_image(img_path, json_path, n)
-
+    
+    R = np.loadtxt("cali/rotate.csv",delimiter=",")
     for i in range(n):
+        filename = os.path.splitext(os.path.basename(img_path))[0]
+        filename, frame = filename.split('_')[-2:]
+        
         input_imgs[i] = np.expand_dims(input_imgs[i], 0)
 
         joints, verts, cams, joints3d, theta = model.predict(input_imgs[i], get_theta=True)
 
-        x,z = calibration(proc_params[i])
+        x,z,p = calibration(proc_params[i], int(frame), i, n)
 
         # center = [0.5*img.shape[1],0.5*img.shape[0]]
         # x = (x - center[0])*0.01
         # z = (z - center[1])*0.01
-
+        
+        # Rotate
+        for k, j in enumerate(joints3d[0]):
+            joints3d[0][k] = np.matmul(R, j.T).T
+        
+        # Translate
         joints3d[0] += np.array([[x,0,z]]*19)
 
         joints_names = ['Ankle.R_x', 'Ankle.R_y', 'Ankle.R_z',
@@ -131,24 +156,25 @@ def main(img_path, json_path, n):
         joints_export['hip.Center_x'] = hipCenter.iloc[0][::3].sum()/2
         joints_export['hip.Center_y'] = hipCenter.iloc[0][1::3].sum()/2
         joints_export['hip.Center_z'] = hipCenter.iloc[0][2::3].sum()/2
+                    
+        print("hmr/output/csv/"+os.path.splitext(os.path.basename(img_path))[0]+"_%02d.csv"%p)
 
-        print("hmr/output/csv/"+os.path.splitext(os.path.basename(img_path))[0]+"_%02d.csv"%i)
+        joints_export.to_csv("hmr/output/csv/"+os.path.splitext(os.path.basename(img_path))[0]+"_%02d.csv"%p)
 
-        joints_export.to_csv("hmr/output/csv/"+os.path.splitext(os.path.basename(img_path))[0]+"_%02d.csv"%i)
+def distance(p1, p2):
+    return (p1[0]-p2[0])**2 + (p1[1]-p2[1])**2
+
         
-    
-
-
 def join_csv(n):
-  path = 'hmr/output/csv/'                   
-  for i in range(n):
-    all_files = glob.glob(os.path.join(path, "*_{:02d}.csv".format(i)))
+    path = 'hmr/output/csv/'                   
+    for i in range(n):
+        all_files = glob.glob(os.path.join(path, "*_{:02d}.csv".format(i)))
 
-    df_from_each_file = (pd.read_csv(f) for f in sorted(all_files))
-    concatenated_df   = pd.concat(df_from_each_file, ignore_index=True)
+        df_from_each_file = (pd.read_csv(f) for f in sorted(all_files))
+        concatenated_df   = pd.concat(df_from_each_file, ignore_index=True)
 
-    concatenated_df['frame'] = concatenated_df.index+1
-    concatenated_df.to_csv("hmr/output/csv_joined/csv_joined_{:02d}.csv".format(i), index=False)
+        concatenated_df['frame'] = concatenated_df.index+1
+        concatenated_df.to_csv("hmr/output/csv_joined/csv_joined_{:02d}.csv".format(i), index=False)
 
 
 if __name__ == '__main__':
@@ -168,4 +194,4 @@ if __name__ == '__main__':
 
     join_csv(config.num_people)
 
-    print('\nResult is in hmr/output')
+    print('Result is in hmr/output\n')

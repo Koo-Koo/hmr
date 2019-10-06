e906f32ac9f173c522df8b5735ba356dd0e2fb87
Ubuntu <ubuntu@ip-172-31-45-175.us-east-2.compute.internal>
2019-10-06 06:20:24 +0000

    Update: exclude keypoints with low confidence

--- src/util/openpose.py
+++ src/util/openpose.py
@@ -34,21 +34,25 @@ def get_bbox(json_path, vis_thr=0.2):
 
     return scale, center
 
-# TODO return multiple scales, centers
-def get_multiple_bbox(json_path, n = 1, vis_thr=0.2):
+# return multiple scales, centers
+def get_multiple_bbox(json_path, n = 1, vis_thr=0.2):    
     kps = read_json(json_path)
     # Pick top n detections.
     scores = np.array([np.mean(kp[kp[:, 2] > vis_thr, 2]) for kp in kps])
+    scores = np.array(map(lambda x: 0 if np.isnan(x) else x,scores))
     kp = [kps[s] for s in scores.argsort()[-n:][::-1]]
     
     scales = list()
     centers = list()
-
     for i in range(n):
-        vis = kp[i][:,2] > vis_thr
-        vis_kp = kp[i][vis, :2]
-        min_pt = np.min(vis_kp, axis=0)
-        max_pt = np.max(vis_kp, axis=0)
+        try:
+            vis = kp[i][:,2] > vis_thr
+            vis_kp = kp[i][vis, :2]
+            min_pt = np.min(vis_kp, axis=0)
+            max_pt = np.max(vis_kp, axis=0)
+        except:
+            print("Too Low confidence!",i)
+            
         person_height = np.linalg.norm(max_pt - min_pt)
         if person_height == 0:
             print('bad!')
